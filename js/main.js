75ae9e4ff7b27d8227d4e25b1f5fb3f0fd2b07cc
Chris Meurink <cmeurink@competa.com>
2018-10-02 10:36:28 +0200

    changed layout and the way i store the persons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 showAllItems();
-const contactDiv = document.getElementById('contacts');
+const contactDiv = document.getElementById('contactContainer');
 
 function showAllItems () {
-    const url = "https://randomuser.me/api/?results=50";
+    const url = "https://randomuser.me/api/?results=20&nat=NL";
     fetch(url)
     .then(function(response) {
         return response.json();
@@ -11,32 +11,46 @@ function showAllItems () {
         console.log(myJson.results);
         for (let i = 0; i < myJson.results.length; i++) {
             const element = myJson.results[i];
-            const link = "<a class='contact' onclick='toggle_visibility(`contactInfo_" + i + "`)' href=#>" + element.name.first + " " + element.name.last + "</a>";
-            // this.addEventListener('click', toggleInfo(this));
-            contactDiv.innerHTML += "<div id='contactInfo_" + i + "' class='contactInfo '> <a class='closeButton' href=# onclick='toggle_visibility(`contactInfo_" + i + "`)'>X</a><h1>" + element.name.first + " " + element.name.last + "</h1> <table><tr><th>Address</th><th>Phone number</th><th>Age</th></tr> <tr><td>" + element.location.street + "</td><td>" + element.phone + "</td><td>" + element.dob.age + "</td></tr></table><img src='" + element.picture.large + "'</div>";
+            const link = `<a class='contact' onclick="getPersonFromLocalStorage('person${i}')" href=#><img src='${element.picture.thumbnail}' /><p>${element.name.first}  ${element.name.last}</p></a>`;
             contactDiv.innerHTML += link;
+            localStorage.setItem(`person${i}`, JSON.stringify(element));
+            console.log(JSON.parse(localStorage.getItem(`person${i}`)));
         }
-
-    })
-    // .then(function(){
-    //     document.getElementsByClassName('contact').addEventListener('click', toggleInfo(document.getElementById('contactToggle').dataset.contactId));
-    // });
+    });
 }
 
-function toggle_visibility(id) {
-    console.log(id);
-    document.getElementsByClassName('contactInfo').visibility = 'hidden';
+function getPersonFromLocalStorage(id) {
+    let person = JSON.parse(localStorage.getItem(id));
+    let userInfo = document.getElementById('userInfo');
+    userInfo.innerHTML = "";
+    userInfo.innerHTML += `
+        <img src="${person.picture.large}" alt="Picture of person">
+        <div class="info">
+            <h1>${person.name.first} ${person.name.last}</h1>
+            <table>
+                <tr>
+                    <th>Phone number</th>
+                </tr>
+                <tr>
+                    <td>${person.cell}</td>
+                </tr>
+                <tr>
+                    <th>E-mail address</th>
+                </tr>
+                <tr>
+                    <td>${person.email}</td>
+                </tr>
+                <tr>
+                    <th>Address</th>
+                </tr>
+                <tr>
+                    <td>${person.location.street}</td>
+                </tr>
+            </table>
+        </div>
+    `;
+    console.log(person);
     
-    var e = document.getElementById(id);
-    if(e.style.visibility == 'visible')
-       e.style.visibility = 'hidden';
-    else
-       e.style.visibility = 'visible';
+    return person;
  }
-
-function toggleInfo(id){
-    console.log(id);
-    let divToToggle = document.getElementsByClassName("contactInfo_" + id);
-    console.log("divToToggle: "+divToToggle);
-}
 
